a4630dbf815b268dd2580b675c13337cd42d9a3a
sergei-1973 <baragozik00@gmail.com>
2025-01-03 11:32:16 +0300

    add element

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 document.body.innerHTML = "<h2>Hello from JavaScript<h2>";
 console.log("Test 2");
+
+function ceateElement(text) {
+  const element = document.createElement("h2");
+  element.textContent = text;
+  return element;
+}
